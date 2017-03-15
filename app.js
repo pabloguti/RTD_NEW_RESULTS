37429da46eca4d56195d337d24ad4c108d189415
Pablo Gutierrez <pablo.gutierrezdiez@cognizant.com>
2017-03-15 16:00:58 +0100

    Io sockets on

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ server.listen(config.port, function () {
 var io = require('socket.io').listen(server);
 
 //ENDPOINT WEBSOCKETS
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
   io.set('transports', ['websocket']);
   socket.on('question', function(question){
     console.log('Question selected: ' + question.selection);
